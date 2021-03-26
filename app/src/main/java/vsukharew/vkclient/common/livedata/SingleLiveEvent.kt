a1965fd116827977dbc18e8c59eac13fbba86db8
vsukharew <vadimsukharev1@yandex.ru>
2021-03-26 21:58:11 +0300

    renamed SingleLiveData, added SingleLiveEvent

--- app/src/main/java/vsukharew/vkclient/common/livedata/SingleLiveEvent.kt
+++ app/src/main/java/vsukharew/vkclient/common/livedata/SingleLiveEvent.kt
@@ -1,58 +1,27 @@
 package vsukharew.vkclient.common.livedata
 
-import android.util.Log
-import androidx.annotation.MainThread
-import androidx.annotation.Nullable
-import androidx.lifecycle.LifecycleOwner
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
-import java.util.concurrent.atomic.AtomicBoolean
-
-
 /**
- * A lifecycle-aware observable that sends only new updates after subscription, used for events like
- * navigation and Snackbar messages.
- *
- *
- * This avoids a common problem with events: on configuration change (like rotation) an update
- * can be emitted if the observer is active. This LiveData only calls the observable if there's an
- * explicit call to setValue() or call().
- *
- *
- * Note that only one observer is going to be notified of changes.
+ * Used as a wrapper for data that is exposed via a LiveData that represents an event.
  */
-class SingleLiveEvent<T> : MutableLiveData<T?>() {
-    private val mPending: AtomicBoolean = AtomicBoolean(false)
+open class SingleLiveEvent<out T>(private val content: T) {
 
-    override fun observe(owner: LifecycleOwner, observer: Observer<in T?>) {
-        if (hasActiveObservers()) {
-            Log.w(TAG, "Multiple observers registered but only one will be notified of changes.")
+    var hasBeenHandled = false
+        private set // Allow external read but not write
+
+    /**
+     * Returns the content and prevents its use again.
+     */
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
         }
-
-        // Observe the internal MutableLiveData
-        super.observe(owner, {
-            if (mPending.compareAndSet(true, false)) {
-                observer.onChanged(it)
-            }
-        })
-    }
-
-    @MainThread
-    override fun setValue(@Nullable t: T?) {
-        mPending.set(true)
-        super.setValue(t)
     }
 
     /**
-     * Used for cases where T is Void, to make calls cleaner.
+     * Returns the content, even if it's already been handled.
      */
-    @MainThread
-    fun call() {
-        value = null
-    }
-
-    private companion object {
-        private const val TAG = "SingleLiveEvent"
-    }
+    fun peekContent(): T = content
 }

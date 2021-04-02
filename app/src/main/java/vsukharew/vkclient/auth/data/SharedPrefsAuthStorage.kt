64ff3d7d9d403641e24d4c64818c5299e82aa767
vsukharew <vadimsukharev1@yandex.ru>
2021-04-02 14:08:11 +0300

    using the single key instance that is stored in Android KeyStore

--- app/src/main/java/vsukharew/vkclient/auth/data/SharedPrefsAuthStorage.kt
+++ app/src/main/java/vsukharew/vkclient/auth/data/SharedPrefsAuthStorage.kt
@@ -1,35 +1,30 @@
 package vsukharew.vkclient.auth.data
 
 import android.content.Context
-import android.content.SharedPreferences
 import android.util.Base64
-import com.google.crypto.tink.Aead
-import com.google.crypto.tink.KeysetHandle
-import com.google.crypto.tink.aead.AesGcmKeyManager
+import com.google.crypto.tink.integration.android.AndroidKeystoreKmsClient
 import vsukharew.vkclient.auth.domain.model.Token
 import vsukharew.vkclient.auth.domain.model.Token.Companion.ACCESS_TOKEN_KEY
 import vsukharew.vkclient.auth.domain.model.Token.Companion.EXPIRES_IN_KEY
-import vsukharew.vkclient.auth.domain.model.Token.Companion.INVALID_TOKEN
+import java.nio.charset.Charset
 
 class SharedPrefsAuthStorage(context: Context) : AuthStorage {
-    private val prefs: SharedPreferences = context.getSharedPreferences(AUTH_PREFS, Context.MODE_PRIVATE)
-    private val aead = KeysetHandle.generateNew(AesGcmKeyManager.aes128GcmTemplate())
-        .getPrimitive(Aead::class.java)
+    private val prefs = context.getSharedPreferences(AUTH_PREFS, Context.MODE_PRIVATE)
+    private val aead = AndroidKeystoreKmsClient.getOrGenerateNewAeadKey(KEY_URI)
+    private val associatedData: ByteArray = byteArrayOf()
 
     override suspend fun getToken(): Token? {
         val accessToken = prefs.getString(ACCESS_TOKEN_KEY, null)
             ?.let { Base64.decode(it, Base64.DEFAULT) }
-            ?.let { aead.decrypt(it, null) }
-            ?.let { String(it, Charsets.UTF_8) }
+            ?.let { aead.decrypt(it, associatedData) }
+            ?.let { String(it, Charset.defaultCharset()) }
         val expiredIn = prefs.getLong(EXPIRES_IN_KEY, Token.INVALID_EXPIRES_IN)
         return accessToken?.let { Token(it, expiredIn) }
     }
 
     override suspend fun putToken(token: Token) {
-        val encryptedAccessToken = aead.encrypt(
-            token.accessToken.toByteArray(),
-            null
-        ).let { Base64.encodeToString(it, Base64.DEFAULT) }
+        val encryptedAccessToken = aead.encrypt(token.accessToken.toByteArray(), associatedData)
+            .let { Base64.encodeToString(it, Base64.DEFAULT) }
         prefs.edit()
             .putString(ACCESS_TOKEN_KEY, encryptedAccessToken)
             .putLong(EXPIRES_IN_KEY, token.expiresIn)
@@ -38,5 +33,6 @@ class SharedPrefsAuthStorage(context: Context) : AuthStorage {
 
     private companion object {
         private const val AUTH_PREFS = "auth_prefs"
+        private const val KEY_URI = "android-keystore://auth_storage_key"
     }
 }

dfbe75674872177d9109d498fa2ab055a2c20ac7
vsukharew <vadimsukharev1@yandex.ru>
2021-04-04 15:28:39 +0300

    added the logic that checks if a user is authorized

--- app/src/main/java/vsukharew/vkclient/auth/domain/interactor/AuthInteractor.kt
+++ app/src/main/java/vsukharew/vkclient/auth/domain/interactor/AuthInteractor.kt
@@ -3,5 +3,6 @@ package vsukharew.vkclient.auth.domain.interactor
 import vsukharew.vkclient.auth.domain.model.Token
 
 interface AuthInteractor {
+    suspend fun isAuthorized(): Boolean
     suspend fun saveToken(token: Token)
 }

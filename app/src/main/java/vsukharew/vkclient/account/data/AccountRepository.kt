202925b38d537a3fa686f665d84f4d5e2b3f36cc
vsukharew <vadimsukharev1@yandex.ru>
2021-04-07 16:12:31 +0300

    changed return type for profile info request

--- app/src/main/java/vsukharew/vkclient/account/data/AccountRepository.kt
+++ app/src/main/java/vsukharew/vkclient/account/data/AccountRepository.kt
@@ -2,10 +2,19 @@ package vsukharew.vkclient.account.data
 
 import vsukharew.vkclient.account.data.network.AccountApi
 import vsukharew.vkclient.account.domain.model.ProfileInfo
+import vsukharew.vkclient.common.extension.EMPTY
+import vsukharew.vkclient.common.extension.map
+import vsukharew.vkclient.common.domain.model.Result
 
 class AccountRepository(private val accountApi: AccountApi) : AccountRepo {
 
-    override suspend fun getProfileInfo(): ProfileInfo {
-        return accountApi.getProfileInfo().let { ProfileInfo(it.response.firstName, it.response.lastName) }
+    override suspend fun getProfileInfo(): Result<ProfileInfo> {
+        return accountApi.getProfileInfo()
+            .map {
+                ProfileInfo(
+                    it.response?.firstName ?: String.EMPTY,
+                    it.response?.lastName ?: String.EMPTY
+                )
+            }
     }
 }
--- app/src/main/java/vsukharew/vkclient/account/data/network/AccountApi.kt
+++ app/src/main/java/vsukharew/vkclient/account/data/network/AccountApi.kt
@@ -2,11 +2,12 @@ package vsukharew.vkclient.account.data.network
 
 import retrofit2.http.GET
 import vsukharew.vkclient.account.data.model.ProfileInfoResponse
-import vsukharew.vkclient.common.network.Response
+import vsukharew.vkclient.common.network.response.ResponseWrapper
+import vsukharew.vkclient.common.domain.model.Result
 import vsukharew.vkclient.common.network.ServerUrls.Account.GET_PROFILE
 
 interface AccountApi {
 
     @GET(GET_PROFILE)
-    suspend fun getProfileInfo(): Response<ProfileInfoResponse>
+    suspend fun getProfileInfo(): Result<ResponseWrapper<ProfileInfoResponse>>
 }

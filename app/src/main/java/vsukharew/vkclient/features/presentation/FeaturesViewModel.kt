c3d1b45cf5b7e9f1ac26bc591d27efc81fa4bad2
vsukharew <vadimsukharev1@yandex.ru>
2021-04-14 13:36:23 +0300

    fixed session clear after alert dialog closure

--- app/src/main/java/vsukharew/vkclient/features/presentation/FeaturesViewModel.kt
+++ app/src/main/java/vsukharew/vkclient/features/presentation/FeaturesViewModel.kt
@@ -7,7 +7,6 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import vsukharew.vkclient.account.data.AccountRepo
 import vsukharew.vkclient.auth.domain.interactor.AuthInteractor
-import vsukharew.vkclient.auth.domain.model.AuthType
 import vsukharew.vkclient.auth.domain.model.AuthType.APP
 import vsukharew.vkclient.auth.domain.model.AuthType.BROWSER
 import vsukharew.vkclient.common.domain.interactor.SessionInteractor
@@ -33,7 +32,8 @@ class FeaturesViewModel(
     val isLoading: MutableLiveData<Boolean> = MutableLiveData(false)
 
     val signOutEvent = MutableLiveData<SingleLiveEvent<Unit>>()
-    val signOutDialogEvent = MutableLiveData<SingleLiveEvent<Unit>>()
+    val signOutDialogEvent = MutableLiveData<Unit>()
+    val signOutDialogClosedEvent = MutableLiveData<SingleLiveEvent<Unit>>()
 
     fun onSignOutClick() {
         viewModelScope.launch {
@@ -43,12 +43,19 @@ class FeaturesViewModel(
                     signOutEvent.value = SingleLiveEvent(Unit)
                 }
                 BROWSER -> {
-                    signOutDialogEvent.value = SingleLiveEvent(Unit)
+                    signOutDialogEvent.value = Unit
                 }
                 else -> {
                     // do nothing
                 }
             }
+        }
+    }
+
+    fun onSignOutDialogClosed() {
+        viewModelScope.launch {
+            sessionInteractor.clearSessionData()
+            signOutDialogClosedEvent.value = SingleLiveEvent(Unit)
         }
     }
 }

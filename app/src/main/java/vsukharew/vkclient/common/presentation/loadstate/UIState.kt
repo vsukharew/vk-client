027a7241b2e117d3dc9c1dc639eb53e2d8dc30f3
vsukharew <vadimsukharev1@yandex.ru>
2021-04-16 15:34:53 +0300

    changed type of the data in UIState.Success to T

--- app/src/main/java/vsukharew/vkclient/common/presentation/loadstate/UIState.kt
+++ app/src/main/java/vsukharew/vkclient/common/presentation/loadstate/UIState.kt
@@ -6,7 +6,7 @@ import vsukharew.vkclient.common.livedata.SingleLiveEvent
 sealed class UIState<out T> {
     object LoadingProgress : UIState<Nothing>()
     object SwipeRefreshProgress : UIState<Nothing>()
-    data class Success<T>(val data: Result.Success<T>): UIState<T>()
+    data class Success<T>(val data: T): UIState<T>()
     data class Error(val error: SingleLiveEvent<Result.Error>): UIState<Nothing>()
     data class SwipeRefreshError(val error: SingleLiveEvent<Result.Error>): UIState<Nothing>()
 }

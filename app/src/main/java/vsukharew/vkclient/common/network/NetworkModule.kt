ef5cdd76c5af6b3a94c12d042af66f2c83394d9c
vsukharew <vadimsukharev1@yandex.ru>
2021-04-16 15:48:12 +0300

    added deserializer for return type of checking short name availability method

--- app/src/main/java/vsukharew/vkclient/common/network/NetworkModule.kt
+++ app/src/main/java/vsukharew/vkclient/common/network/NetworkModule.kt
@@ -1,15 +1,25 @@
 package vsukharew.vkclient.common.network
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import vsukharew.vkclient.BuildConfig
+import vsukharew.vkclient.account.data.model.ScreenNameResponse
 import vsukharew.vkclient.auth.data.AuthStorage
 import vsukharew.vkclient.common.network.calladapter.ResultAdapterFactory
+import vsukharew.vkclient.common.network.deserializer.ResolvedScreenNameDeserializer
 import vsukharew.vkclient.common.network.interceptor.AddTokenInterceptor
 import java.util.concurrent.TimeUnit
+
+private fun provideGson(): Gson {
+    return GsonBuilder()
+        .registerTypeAdapter(ScreenNameResponse::class.java, ResolvedScreenNameDeserializer())
+        .create()
+}
 
 private fun provideOkHttpClient(authStorage: AuthStorage): OkHttpClient {
     return OkHttpClient.Builder()
@@ -31,7 +41,7 @@ private fun provideRetrofit(authStorage: AuthStorage): Retrofit {
         .baseUrl(ServerUrls.BASE_URL)
         .client(provideOkHttpClient(authStorage))
         .addCallAdapterFactory(ResultAdapterFactory())
-        .addConverterFactory(GsonConverterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create(provideGson()))
         .build()
 }
 

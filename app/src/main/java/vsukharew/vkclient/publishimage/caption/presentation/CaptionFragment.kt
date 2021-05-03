efe85f0fa613a9d71022abd1dc128d397930789e
vsukharew <vadimsukharev1@yandex.ru>
2021-05-03 10:19:33 +0300

    enabled/disabled button depending on entered text

--- app/src/main/java/vsukharew/vkclient/publishimage/caption/presentation/CaptionFragment.kt
+++ app/src/main/java/vsukharew/vkclient/publishimage/caption/presentation/CaptionFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.OnBackPressedCallback
 import androidx.core.view.isVisible
+import androidx.core.widget.doOnTextChanged
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import vsukharew.vkclient.R
@@ -43,6 +44,9 @@ class CaptionFragment : BaseFragment<FragmentCaptionBinding>(R.layout.fragment_c
 
     private fun setListeners() {
         binding.apply {
+            captionText.doOnTextChanged { text, _, _, _ ->
+                publish.isEnabled = text?.isNotEmpty() ?: false
+            }
             publish.setOnClickListener {
                 captionText.text?.toString()?.let(viewModel::publish)
             }

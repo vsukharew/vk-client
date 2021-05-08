cfed9a8bcb934aa3c7e241a9c9d4437f06a9754b
vsukharew <vadimsukharev1@yandex.ru>
2021-05-08 17:48:55 +0300

    added @FlowPreview annotation

--- app/src/main/java/vsukharew/vkclient/features/presentation/FeaturesFragment.kt
+++ app/src/main/java/vsukharew/vkclient/features/presentation/FeaturesFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -28,6 +29,7 @@ import vsukharew.vkclient.features.navigation.FeaturesCoordinator
 import vsukharew.vkclient.screenname.model.ScreenNameAvailability
 import vsukharew.vkclient.screenname.model.ScreenNameAvailability.*
 
+@FlowPreview
 class FeaturesFragment : BaseFragment<FragmentFeaturesBinding>(R.layout.fragment_features) {
     private val viewModel: FeaturesViewModel by viewModel()
     private val featuresCoordinator: FeaturesCoordinator by inject()
@@ -54,7 +56,7 @@ class FeaturesFragment : BaseFragment<FragmentFeaturesBinding>(R.layout.fragment
             shortNameUiState.observe(viewLifecycleOwner, ::observeShortNameUiState)
             shortNameTextState.observe(viewLifecycleOwner, ::observeShortNameTextState)
             signOutEvent.observe(viewLifecycleOwner, ::observeSignOutEvent)
-            signOutDialogEvent.observe(viewLifecycleOwner, ::observeSignOutDialogEvent)
+            signOutDialogEvent.observe(viewLifecycleOwner, { observeSignOutDialogEvent() })
             signOutDialogClosedEvent.observe(viewLifecycleOwner, ::observeSignOutEvent)
             postPublishedEvent.observe(viewLifecycleOwner, ::observePublishedPosts)
         }
@@ -116,7 +118,7 @@ class FeaturesFragment : BaseFragment<FragmentFeaturesBinding>(R.layout.fragment
         }
     }
 
-    private fun observeSignOutDialogEvent(event: Unit) {
+    private fun observeSignOutDialogEvent() {
         AlertDialog.Builder(requireContext())
             .setMessage(R.string.features_fragment_sign_out_dialog_text)
             .setPositiveButton(R.string.ok_text) { _, _ ->

4f0ff68c8bebaa4e439a5d5a9b950a8bd4879d64
vsukharew <vadimsukharev1@yandex.ru>
2023-04-29 20:13:31 +0300

    added default values

--- app/src/main/java/vsukharew/vkclient/publishimage/attach/data/model/WallUploadAddressResponse.kt
+++ app/src/main/java/vsukharew/vkclient/publishimage/attach/data/model/WallUploadAddressResponse.kt
@@ -1,5 +1,6 @@
 package vsukharew.vkclient.publishimage.attach.data.model
 
 import com.google.gson.annotations.SerializedName
+import vsukharew.vkclient.common.network.response.DEFAULT_STRING
 
-data class WallUploadAddressResponse(@SerializedName("upload_url") val uploadUrl: String)
+data class WallUploadAddressResponse(@SerializedName("upload_url") val uploadUrl: String = DEFAULT_STRING)

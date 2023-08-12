78d5b13a671f098c6267026b17545190dbaeba86
vsukharew <vadimsukharev1@yandex.ru>
2023-08-12 19:15:14 +0300

    fixed bug when after state restoration placeholder was not shown

--- app/src/main/java/vsukharew/vkclient/publishimage/attach/presentation/model/UIImage.kt
+++ app/src/main/java/vsukharew/vkclient/publishimage/attach/presentation/model/UIImage.kt
@@ -5,7 +5,13 @@ import vsukharew.vkclient.publishimage.attach.domain.model.Image
 import java.io.Serializable
 
 sealed class UIImage : Serializable {
-    object AddNewImagePlaceholder : UIImage()
+    object AddNewImagePlaceholder : UIImage() {
+        // equals is overridden because deserialization creates a singleton
+        // that has a different address comparing with the one that was accessed via class name
+        override fun equals(other: Any?): Boolean {
+            return other is AddNewImagePlaceholder
+        }
+    }
     data class RealImage(
         val image: Image,
         val loadingState: ImageLoadingState
